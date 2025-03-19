cc147e166340ce8efef87e57e6c88a0f0b65f3d2
Denys <prokopiuckdenys250904@gmail.com>
2025-03-19 19:51:28 +0200

    Update

--- Lab3/script.js
+++ Lab3/script.js
@@ -10,4 +10,15 @@
             helloSpeak.speak(name)
         }
     }
+
+    console.log("Якщо кількість букв в імені парна - \" was kicked for bad joke\", а якщо непарна - \"You are beautiful person \"");
+
+    for(let i = 0; i < names.length; i++){
+        let name = names[i];
+        if(name.length % 2 == 0){
+            console.log(name + " was kicked for bad joke");
+        }else{
+            console.log("You are beautiful person " + name);
+        }
+    }
 })();
